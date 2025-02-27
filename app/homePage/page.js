c2688c2d11b8d81653ccead6b36bc084f85cce7b
Bloomer004 <ishaguptavnsv@gmail.com>
2025-02-27 21:47:34 +0530

    created Landing Page

--- app/homePage/page.js
+++ app/homePage/page.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Image from 'next/image'
 import LandingPage from '@/sections/LandingPage'
 
-// “The Doctor of the future will give no medicine, but will instruct his patient in
 const HomePage = () => {
     return (
         <div>
