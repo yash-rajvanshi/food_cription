337757c4836d850ddf8aece0dfffee578f1034c9
Bloomer004 <ishaguptavnsv@gmail.com>
2025-02-28 00:25:03 +0530

    How it works page done

--- sections/Strategy.js
+++ sections/Strategy.js
@@ -12,24 +12,194 @@ const Strategy = () => {
                     </div>
                 </div>
                 <div className='flex py-5 w-11/12 mx-auto'>
-                    <div className='flex flex-col max-width-[100%] justify-evenly'>
+                    <div className='flex flex-col max-w-[100%] justify-evenly'>
                         <div className='flex rounded-l-full bg-gradient-to-b from-[#FFF] via-[#FFF] to-[#F5F5F5] w-full items-center justify-between'>
                             <div className='flex justify-start w-full'>
-                                <Image src="/write.svg" alt="write" width={100} height={100}/>
+                                <Image src="/write.svg" alt="write" width={100} height={100} />
                                 <div className='flex flex-col w-[50%]'>
                                     <h2 className='font-extrabold text-4xl text-[#64EDCC]'>01</h2>
                                     <p className=''>Enter your details - Get FoodCription</p>
                                     <div className='flex flex-row justify-between items-center w-[40%]'>
-                                        <div className='bg-[#64EDCC]'></div>
-                                        <div className='bg-[#92ADCC]'></div>
-                                        <div className='bg-[#FF965A]'></div>
-                                        <div className='bg-[#A4D221]'></div>
-                                        <div className='bg-[#F6DD5E]'></div>
-                                        <div className='bg-[#FB76BE]'></div>
+                                        <div className='bg-[#64EDCC] w-2 h-2 rounded-full'></div>
+                                        <div className='bg-[#92ADCC] w-1 h-1 rounded-full'></div>
+                                        <div className='bg-[#FF965A] w-1 h-1 rounded-full'></div>
+                                        <div className='bg-[#A4D221] w-1 h-1 rounded-full'></div>
+                                        <div className='bg-[#F6DD5E] w-1 h-1 rounded-full'></div>
+                                        <div className='bg-[#FB76BE] w-1 h-1 rounded-full'></div>
                                     </div>
                                 </div>
                             </div>
-                            <div></div>
+                            <div className='flex flex-col h-max justify-between w-[5%] items-center'>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                            </div>
+                        </div>
+
+                        <div className='flex rounded-l-full bg-gradient-to-b from-[#FFF] via-[#FFF] to-[#F5F5F5] w-full items-center justify-between'>
+                            <div className='flex justify-start w-full'>
+                                <Image src="/step3_yoga.svg" alt="write" width={100} height={100} />
+                                <div className='flex flex-col w-[50%]'>
+                                    <h2 className='font-extrabold text-4xl text-[#FF965A]'>03</h2>
+                                    <p className=''>Yoga, Exercise, Meditation Advice</p>
+                                    <div className='flex flex-row justify-between items-center w-[40%]'>
+                                        <div className='bg-[#64EDCC] w-1 h-1 rounded-full'></div>
+                                        <div className='bg-[#92ADCC] w-1 h-1 rounded-full'></div>
+                                        <div className='bg-[#FF965A] w-2 h-2 rounded-full'></div>
+                                        <div className='bg-[#A4D221] w-1 h-1 rounded-full'></div>
+                                        <div className='bg-[#F6DD5E] w-1 h-1 rounded-full'></div>
+                                        <div className='bg-[#FB76BE] w-1 h-1 rounded-full'></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='flex flex-col h-max justify-between w-[5%] items-center'>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                            </div>
+
+                        </div>
+
+                        <div className='flex rounded-l-full bg-gradient-to-b from-[#FFF] via-[#FFF] to-[#F5F5F5] w-full items-center justify-between'>
+                            <div className='flex justify-start w-full'>
+                                <Image src="/step5_meals.svg" alt="write" width={100} height={100} />
+                                <div className='flex flex-col w-[50%]'>
+                                    <h2 className='font-extrabold text-4xl text-[#F6DD5E]'>05</h2>
+                                    <p className=''>Option to buy ready healthy meals</p>
+                                    <div className='flex flex-row justify-between items-center w-[40%]'>
+                                        <div className='bg-[#64EDCC] w-1 h-1 rounded-full'></div>
+                                        <div className='bg-[#92ADCC] w-1 h-1 rounded-full'></div>
+                                        <div className='bg-[#FF965A] w-1 h-1 rounded-full'></div>
+                                        <div className='bg-[#A4D221] w-1 h-1 rounded-full'></div>
+                                        <div className='bg-[#F6DD5E] w-2 h-2 rounded-full'></div>
+                                        <div className='bg-[#FB76BE] w-1 h-1 rounded-full'></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='flex flex-col h-max justify-between w-[5%] items-center'>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='w-[30%] px-2'>
+                        <Image src="/dummy_foodcription.png" alt='dummy_foodcription' width={420} height={850} />
+                    </div>
+                    <div className='flex flex-col max-w-[100%] justify-evenly'>
+                        <div className='flex rounded-r-full bg-gradient-to-b from-[#FFF] via-[#FFF] to-[#F5F5F5] w-full items-center justify-between'>
+                            <div className='flex flex-col h-max justify-between w-[5%] items-center'>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                            </div>
+                            <div className='flex justify-start w-full'>
+                                <Image src="/step2_grocery.svg" alt="write" width={100} height={100} />
+                                <div className='flex flex-col w-[50%]'>
+                                    <h2 className='font-extrabold text-4xl text-[#92ADCC]'>02</h2>
+                                    <p className=''>Get personalized Grocery List</p>
+                                    <div className='flex flex-row justify-between items-center w-[40%]'>
+                                        <div className='bg-[#64EDCC] w-1 h-1 rounded-full'></div>
+                                        <div className='bg-[#92ADCC] w-2 h-2 rounded-full'></div>
+                                        <div className='bg-[#FF965A] w-1 h-1 rounded-full'></div>
+                                        <div className='bg-[#A4D221] w-1 h-1 rounded-full'></div>
+                                        <div className='bg-[#F6DD5E] w-1 h-1 rounded-full'></div>
+                                        <div className='bg-[#FB76BE] w-1 h-1 rounded-full'></div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div className='flex rounded-r-full bg-gradient-to-b from-[#FFF] via-[#FFF] to-[#F5F5F5] w-full items-center justify-between'>
+                            <div className='flex flex-col h-max justify-between w-[5%] items-center'>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                            </div>
+                            <div className='flex justify-start w-full'>
+                                <Image src="/step4_orders.svg" alt="write" width={100} height={100} />
+                                <div className='flex flex-col w-[50%]'>
+                                    <h2 className='font-extrabold text-4xl text-[#A4D221]'>03</h2>
+                                    <p className=''>Grocery Buying option from our health partners</p>
+                                    <div className='flex flex-row justify-between items-center w-[40%]'>
+                                        <div className='bg-[#64EDCC] w-1 h-1 rounded-full'></div>
+                                        <div className='bg-[#92ADCC] w-1 h-1 rounded-full'></div>
+                                        <div className='bg-[#FF965A] w-1 h-1 rounded-full'></div>
+                                        <div className='bg-[#A4D221] w-2 h-2 rounded-full'></div>
+                                        <div className='bg-[#F6DD5E] w-1 h-1 rounded-full'></div>
+                                        <div className='bg-[#FB76BE] w-1 h-1 rounded-full'></div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                        </div>
+
+                        <div className='flex rounded-r-full bg-gradient-to-b from-[#FFF] via-[#FFF] to-[#F5F5F5] w-full items-center justify-between'>
+                            <div className='flex flex-col h-max justify-between w-[5%] items-center'>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                                <div className='bg-gradient-to-r from-[#EAEEF0] to-[#8F8F8F] w-1 h-1 rounded-full mb-1'></div>
+                            </div>
+                            <div className='flex justify-start w-full'>
+                                <Image src="/step6_goal.svg" alt="write" width={100} height={100} />
+                                <div className='flex flex-col w-[50%]'>
+                                    <h2 className='font-extrabold text-4xl text-[#FB76BE]'>05</h2>
+                                    <p className=''>Health Goals Fulfilled in the most convenient way</p>
+                                    <div className='flex flex-row justify-between items-center w-[40%]'>
+                                        <div className='bg-[#64EDCC] w-1 h-1 rounded-full'></div>
+                                        <div className='bg-[#92ADCC] w-1 h-1 rounded-full'></div>
+                                        <div className='bg-[#FF965A] w-1 h-1 rounded-full'></div>
+                                        <div className='bg-[#A4D221] w-1 h-1 rounded-full'></div>
+                                        <div className='bg-[#F6DD5E] w-1 h-1 rounded-full'></div>
+                                        <div className='bg-[#FB76BE] w-2 h-2 rounded-full'></div>
+                                    </div>
+                                </div>
+                            </div>
+
                         </div>
                     </div>
                 </div>
