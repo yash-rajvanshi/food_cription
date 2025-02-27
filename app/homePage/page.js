6303bde6287cad27ffad5a2fc2d62ff78fdf45c4
Bloomer004 <ishaguptavnsv@gmail.com>
2025-02-27 22:04:17 +0530

    created Landing Page

--- app/homePage/page.js
+++ app/homePage/page.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Image from 'next/image'
 import LandingPage from '@/sections/LandingPage'
+import FourPillars from '@/sections/FourPillars'
 
 const HomePage = () => {
     return (
         <div>
             <LandingPage/>
+            <FourPillars/>
         </div>
     )
 }
---
+++ sections/FourPillars.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const FourPillars = () => {
+  return (
+    <div>
+      <div className='w-full py-10 '>
+        <div className='flex justify-center items-center py-5'>
+            <div className='flex flex-col items-center w-full max-w-[1200px]'>
+                <div className='flex flex-col items-center justify-center'>
+                    <h1 className='text-gray-800 font-medium text-3xl'>4 PILLARS of Our Well Being</h1>
+                    <div className='text-gray-700 font-normal'>Embark on a personalized journey to optimal well-being with our free, practical, and results-driven lifestyle prescriptions.</div>
+                </div>
+                <div className='flex justify-evenly w-full py-5'>
+                    <div className='flex flex-col items-center cursor-pointer'>
+                        <div className='w-[100px] h-[100px] flex flex-col border-2 rounded-lg p-4 border-gray-300/30 hover:border-teal-500'>
+                            <Image/>
+                        </div>
+                        <h2 className='flex justify-center font-semibold'>Diet</h2>
+                    </div>
+                    <div className='flex flex-col items-center cursor-pointer'></div>
+                    <div className='flex flex-col items-center cursor-pointer'></div>
+                    <div className='flex flex-col items-center cursor-pointer'></div>
+                </div>
+            </div>
+
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default FourPillars
