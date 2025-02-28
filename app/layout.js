4d3a9a306711517fd00716583cb93c94b28839fd
Bloomer004 <ishaguptavnsv@gmail.com>
2025-02-28 20:59:47 +0530

    Contact page done

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Zain, Karla, League_Script } from "next/font/google";
 
+
 const karla = Karla({ subsets: ["latin"], weight: ["400", "700"], variable: "--font-karla" });
 const leagueScript = League_Script({ subsets: ["latin"], weight: ["400"], variable: "--font-league-script" });
 const zain = Zain({ subsets: ["latin"], weight: ["200","300","400","800", "700","900"], variable: "--font-zain" });
@@ -13,7 +14,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={`${karla.className} ${zain.variable} ${leagueScript.variable}`}>
+    <html lang="en" className={`${karla.className} ${zain.variable} ${leagueScript.variable} custom-scrollbar`}>
       <body
         className={`antialiased`}
       >
